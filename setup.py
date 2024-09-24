ca85ef4eb2404fc16846f260c9f7fe52a2f64b2a
Naveen <mr.navi8680@gmail.com>
2024-09-25 00:08:09 +0530

    extra files created

--- setup.py
+++ setup.py
@@ -1,28 +1,28 @@
-from setuptools import find_packages,setup
+from setuptools import find_packages, setup
 from typing import List
 
-HYPEN_E_DOT='-e .'
-def get_requirements(file_path:str)->List[str]:
-    '''
-    this function will return the list of requirements
-    '''
-    requirements=[]
-    with open(file_path) as file_obj:
-        requirements=file_obj.readlines()
-        requirements=[req.replace("\n","") for req in requirements]
+HYPHEN_E_DOT = '-e .'
 
-        if HYPEN_E_DOT in requirements:
-            requirements.remove(HYPEN_E_DOT)
+def get_requirements(file_path: str) -> List[str]:
+    """
+    This function returns a list of requirements from a given file.
+    """
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.strip() for req in requirements]
+
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
     
     return requirements
 
 setup(
-name='END-TO-END DEPLOYMENT',   #Project Name
-version='0.0.1',  
-author='Naveen',
-author_email='naveen.ananadhan@gmail.com',
-packages=find_packages(),
-install_requires=get_requirements('requirements.txt')
-
-
+    name='end-to-end-deployment',   # Project Name
+    version='0.0.1',  
+    author='Naveen',
+    author_email='naveen.ananadhan@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+    
 )
